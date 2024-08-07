261325d9745884c8aba1a711cf3a45d6a8cf53bd
Yaroslava Sabitova <slava.sabitova@yandex.ru>
2024-08-07 19:18:35 +0300

    section conditions

--- src/js/components/js_items.js
+++ src/js/components/js_items.js
@@ -1,6 +1,7 @@
 export default function () {
     const screenHeight = document.documentElement.clientHeight;
     const catalogItems = document.querySelectorAll('.js_catalog_item');
+    const conditionsItems = document.querySelectorAll('.js_conditions_item');
 
     function scrolling() {
         for (let i = 0; i < catalogItems.length; i++) {
@@ -12,6 +13,16 @@ export default function () {
             // else {
             //     listItem.classList.remove('active');
             // }
+        }
+
+        for (let i = 0; i < conditionsItems.length; i++) {
+            const condItem = conditionsItems[i];
+
+            if (isPartiallyVisible(condItem)) {
+                condItem.classList.add('active');
+            } else {
+                condItem.classList.remove('active');
+            }
         }
     }
 
