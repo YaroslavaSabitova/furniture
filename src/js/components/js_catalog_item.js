504f53ca6bd8d96a7e20a7b835dbfb10774054a8
Yaroslava Sabitova <slava.sabitova@yandex.ru>
2024-08-07 12:21:03 +0300

    catalog cards js anim on scroll

---
+++ src/js/components/js_catalog_item.js
@@ -0,0 +1,27 @@
+export default function () {
+    const screenHeight = document.documentElement.clientHeight;
+    const catalogItems = document.querySelectorAll('.js_catalog_item');
+
+    function scrolling() {
+        for (let i = 0; i < catalogItems.length; i++) {
+            const listItem = catalogItems[i];
+
+            if (isPartiallyVisible(listItem)) {
+                listItem.classList.add('active');
+            } else {
+                listItem.classList.remove('active');
+            }
+        }
+    }
+
+    function isPartiallyVisible(element) {
+        const elementBoundary = element.getBoundingClientRect();
+        const top = elementBoundary.top;
+        const bottom = elementBoundary.bottom;
+        const height = elementBoundary.height;
+
+        return top + height >= 0 && height + screenHeight > bottom;
+    }
+
+    window.addEventListener('scroll', scrolling);
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,9 @@
 import js_burger from './components/js_burger.js';
 import js_btn_rounded from './components/js_btn_rounded.js';
+import js_catalog_item from './components/js_catalog_item.js';
 
 (function () {
     js_burger();
     js_btn_rounded();
+    js_catalog_item();
 })();
