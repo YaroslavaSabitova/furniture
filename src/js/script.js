f38096ce65d8d5c4052254f6557dc79cc9660244
Yaroslava Sabitova <slava.sabitova@yandex.ru>
2024-08-08 20:58:16 +0300

    add button-up

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@ import js_btn_rounded from './components/js_btn_rounded.js';
 import js_items from './components/js_items.js';
 import js_catalog_btn from './components/js_catalog_btn.js';
 import js_swiper_advantages from './components/js_swiper_advantages.js';
+import js_button_up from './components/js_button_up.js';
 
 (function () {
     js_burger();
@@ -10,4 +11,5 @@ import js_swiper_advantages from './components/js_swiper_advantages.js';
     js_items();
     js_catalog_btn();
     js_swiper_advantages();
+    js_button_up();
 })();
